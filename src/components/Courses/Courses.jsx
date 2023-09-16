735afbb52df7d39af8b4169c69ead12966bf5055
haa-meem <haameemalhamra@gmail.com>
2023-09-16 17:36:49 +0600

    courses section

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { FiDollarSign } from 'react-icons/fi';
+import { BsBook } from 'react-icons/bs';
+
+const Courses = () => {
+    // Define a state variable to store the course data
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        // Fetch the JSON data here (you can replace the URL with your actual JSON source)
+        fetch('fakeData.json')
+            .then((res) => res.json())
+            .then((data) => {
+                // Update the state with the fetched data
+                setCourses(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div className="grid grid-cols-3 gap-3">
+            {courses.map((course) => (
+                <div key={course.course_id} className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src={course.course_image} alt={course.course_name} className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">{course.course_name}</h2>
+                        <p>{course.course_details}</p>
+                        <div className="card-actions flex items-center">
+                            <FiDollarSign></FiDollarSign>
+                            <span>Price: {course.course_price}</span>
+                            <BsBook></BsBook>
+                            <span>Credit: {course.course_credit}hr</span>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Courses;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-300 flex justify-center">
+            <p className="normal-case text-xl">Course Registration</p>
+        </div>
+    );
+};
+
+export default Navbar;
